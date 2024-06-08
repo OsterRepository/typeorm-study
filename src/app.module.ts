0085381de90ab999060bb61857d4d1e255c3bb25
KimMinSu <kms0843@naver.com>
2024-06-09 00:57:31 +0900

    Chore : prettier 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,39 +5,39 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { StudentModdel, TeacherModel } from './entity/person.entity';
 import {
-  AirplaneModel,
-  BookModel,
-  CarModel,
-  ComputerModel,
-  SingleBaseModel,
+    AirplaneModel,
+    BookModel,
+    CarModel,
+    ComputerModel,
+    SingleBaseModel,
 } from './entity/inheritance.entity';
 import { ProfileModel } from './entity/profile.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserModel, ProfileModel]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'typeormstudy',
-      entities: [
-        UserModel,
-        StudentModdel,
-        TeacherModel,
-        BookModel,
-        CarModel,
-        SingleBaseModel,
-        ComputerModel,
-        AirplaneModel,
-        ProfileModel,
-      ],
-      synchronize: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forFeature([UserModel, ProfileModel]),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: '127.0.0.1',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'typeormstudy',
+            entities: [
+                UserModel,
+                StudentModdel,
+                TeacherModel,
+                BookModel,
+                CarModel,
+                SingleBaseModel,
+                ComputerModel,
+                AirplaneModel,
+                ProfileModel,
+            ],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,64 +1,64 @@
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  Generated,
-  OneToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-  VersionColumn
+    Column,
+    CreateDateColumn,
+    Entity,
+    Generated,
+    OneToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    VersionColumn
 } from "typeorm";
 import { ProfileModel } from "./profile.entity";
 
 export enum Role {
-  ADMIN = 'admin',
-  USER = 'user',
+    ADMIN = 'admin',
+    USER = 'user',
 }
 
 @Entity()
 export class UserModel {
-  //   @PrimaryColumn()
-  @PrimaryGeneratedColumn()
-  id: number;
+    //   @PrimaryColumn()
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  email: string;
+    @Column()
+    email: string;
 
-  //   @Column({
-  //     type: 'varchar',
-  //     name: 'title',
-  //     length: 300,
-  //     nullable: true,
-  //     update: true,
-  //     select: true,
-  //     default: 'default value',
-  //     unique: false,
-  //   })
-  //   title: string;
+    //   @Column({
+    //     type: 'varchar',
+    //     name: 'title',
+    //     length: 300,
+    //     nullable: true,
+    //     update: true,
+    //     select: true,
+    //     default: 'default value',
+    //     unique: false,
+    //   })
+    //   title: string;
 
-  @Column({
-    type: 'enum',
-    enum: Role,
-    default: Role.USER,
-  })
-  role: Role;
+    @Column({
+        type: 'enum',
+        enum: Role,
+        default: Role.USER,
+    })
+    role: Role;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 
-  // 데이터가 업데이트 될때마다 1씩 올라간다.
-  // 처음 생서되면 갑은 1이다.
-  // save() 함수가 몇번 불렸는지 기억한다.
-  @VersionColumn()
-  version: number;
+    // 데이터가 업데이트 될때마다 1씩 올라간다.
+    // 처음 생서되면 갑은 1이다.
+    // save() 함수가 몇번 불렸는지 기억한다.
+    @VersionColumn()
+    version: number;
 
-  @Column()
-  @Generated('uuid')
-  additionalId: string;
+    @Column()
+    @Generated('uuid')
+    additionalId: string;
 
-  @OneToOne(() => ProfileModel, (profile) => profile.user)
-  profile: ProfileModel;
+    @OneToOne(() => ProfileModel, (profile) => profile.user)
+    profile: ProfileModel;
 }
